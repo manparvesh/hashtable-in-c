26c9790b0bf398977d6661caef4dfa3868a69e67
Man Parvesh Singh Randhawa <randhawa_m@worksap.co.jp>
2018-04-19 18:42:39 +0800

    some clean up

--- cmake-build-debug/CMakeFiles/hashtable_in_c.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for hashtable_in_c.
-# This may be replaced when dependencies are built.
--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/manparvesh/CLionProjects/hashtable-in-c
-# Build directory: /home/manparvesh/CLionProjects/hashtable-in-c/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(hashtable_test "/home/manparvesh/CLionProjects/hashtable-in-c/cmake-build-debug/hashtable_test")
